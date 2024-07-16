e073d3326585bc42f37acf66bfa5fe1bf0ddd8bc
Justin <justin@voltcreative.com>
2024-07-16 14:20:58 -0400

    Try environment port

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const url = require('url')
 // Spinning the http server and the websocket server
 const server = http.createServer()
 const wsServer = new WebSocketServer({ server })
-const port = 8000
+const port = process.env.PORT || 8000
 server.listen(port, () => {
     console.log(`Websocket server is running on port ${port}`)
 })
