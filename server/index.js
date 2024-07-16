3c6365456dda82dcc877c5233b9fe561ff6218e4
Justin <justin@voltcreative.com>
2024-07-16 14:15:22 -0400

    add websockets

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 
-const { WebSocketServer } = require('ws')
+const { WebSocket, WebSocketServer } = require('ws')
 const http = require('http')
 const { v4: uuidv4 } = require('uuid')
-
+const { client } = require('websocket')
+const url = require('url')
 // Spinning the http server and the websocket server
 const server = http.createServer()
 const wsServer = new WebSocketServer({ server })
@@ -11,35 +12,69 @@ server.listen(port, () => {
     console.log(`Websocket server is running on port ${port}`)
 })
 
+// Maintain active connections here
 const clients = {}
+const users = {}
 
-wsServer.on('connection', function(connection) {
+function broadcastMessage(json) {
+    const data = JSON.stringify(json)
+    for (let userId in clients) {
+        let client = clients[userId].connection
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(data)
+        }
+    }
+}
+
+function handleMessage(message, userId) {
+    const dataFromClient = JSON.parse(message.toString())
+    const json = { type: dataFromClient.type }
+    json.data = {
+        latitude: dataFromClient.latitude,
+        longitude: dataFromClient.longitude,
+        users: users
+    }
+    broadcastMessage(json)
+}
+
+function handleDisconnect(userId) {
+    delete clients[userId]
+    delete users[userId]
+    broadcastMessage({
+        message: 'user disconnected',
+        users: users
+    })
+}
+
+wsServer.on('connection', function(connection, request) {
     const userId = uuidv4()
 
+    // Get the latitude and longitude from the request
+    const { latitude, longitude } = url.parse(request.url, true).query
+
     // Store the new connection
-    clients[userId] = connection
-    console.log(`${userId} connected.`)
+    clients[userId] = {
+        connection
+    }
+    users[userId] = {
+        latitude: latitude,
+        longitude: longitude
+    }
+    console.log(`User ${userId} connected at:`)
+    console.log(`   Latitude:  ${latitude}`)
+    console.log(`   Longitude: ${longitude}`)
+
+    broadcastMessage({ 
+        message: 'user connected',
+        users: users 
+    })
+
+    connection.on('message', (message) => handleMessage(message, userId))
 
     // User disconnected
-    connection.on('close', function() {
-        console.log(`${userId} disconnected.`)
+    connection.on('close', () => {
+        console.log(`goodbye ${userId}`)
+        handleDisconnect(userId)
     })
 
 })
-
-// wsServer.on('request', function (request) {
-//     console.log((new Date()) + ' Received a new connection from origin ' + request.origin + '.')
-
-//     const connection = request.accept(null, request.origin)
-//     clients[userID] = connection
-//     console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients))
-
-//     connection.on('message', function(message) {
-//         console.log('New streamer: ', message.utf8Data)
-
-//         for (key in clients) {
-//             clients[key].sendUTF(message.utf8Data)
-//             console.log('sent mesage to: ', clients[key])
-//         }
-//     })
-// })
